a756955453eb26caaac0173c9dda564f3a5470dd
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2020-12-08 08:32:20 -0600

    Update pong_OOP.py

--- pong_OOP.py
+++ pong_OOP.py
@@ -5,19 +5,19 @@ pygame.init()
 
 class paddle:
 	
-	def __init__(self, x, y, speed, top_boundary, bottom_boundary, hitbox, picture):
+	def __init__(self, x, y, speed, top_boundary, bottom_boundary, hitbox, image):
 		self.x = x
 		self.y = y
 		self.speed = speed
 		self.top_boundary = top_boundary
 		self.bottom_boundary = bottom_boundary
 		self.hitbox = hitbox
-		self.picture = picture
+		self.image = image
 		
 
-	def load_picture(self):
+	def load_image(self):
 		
-		screen.blit(pygame.image.load(self.picture), (self.x, self.y))
+		screen.blit(pygame.image.load(self.image), (self.x, self.y))
 
 
 	def check_boundary(self):
@@ -53,7 +53,9 @@ class paddle:
 
 class Ball:
 
-	def __init__(self, x, y, max_speed, x_speed, y_speed, top_boundary, bottom_boundary, left_boundary, right_boundary, hitbox, picture):
+
+
+	def __init__(self, x, y, max_speed, x_speed, y_speed, top_boundary, bottom_boundary, left_boundary, right_boundary, hitbox, image):
 		self.x = x
 		self.y = y
 		self.max_speed = max_speed
@@ -64,62 +66,55 @@ class Ball:
 		self.left_boundary = left_boundary
 		self.right_boundary = right_boundary
 		self.hitbox = hitbox
-		self.picture = picture
+		self.image = image
 
 
-	def load_picture(self):
-		screen.blit(pygame.image.load(self.picture), (self.x, self.y))
+	def load_image(self):
+		screen.blit(pygame.image.load(self.image), (self.x, self.y))
+
 
 	def move(self):
 		self.x += self.x_speed
 		self.y += self.y_speed
 
 
-	def check_boundary(self):
+	def check_wall_bounce(self):
 		if self.y < self.top_boundary:
 			self.y_speed *= -1
 		if self.y > self.bottom_boundary:
 			self.y_speed *= -1
 
-		if self.x < self.left_boundary:
-			self.x_speed *= -1
-		if self.x > self.right_boundary:
-			self.x_speed *= -1
+
+	def bounce_off_paddle(self):
+		self.x_speed *= -1.1
+		self.x_speed = round(self.x_speed, 1)	
+
 
 	def move_hitbox(self):
 		self.hitbox = [i for i in range(round(self.y), round(self.y) + 32)]
-
-	def bounce(self):
-		self.x_speed *= -1
 
 
 	def check_hitbox(self):
 		if round(self.x) < 75: 	# left paddle main x
 			for i in self.hitbox:
 				if i in leftpaddle.hitbox:
-					self.x_speed *= -1.1
+					self.bounce_off_paddle()
 					break
 
-		if round(self.x) >693: 	# right paddle main x
+		if round(self.x) > 691: 	# right paddle main x
 			for i in self.hitbox:
 				if i in rightpaddle.hitbox:
-					self.x_speed *= -1.1
+					self.bounce_off_paddle()
 					break
 
-	def new_speed(self): # assigns a new x and y speed to the ball
+
+	def new_speed(self): # used for adding more balls
 		self.max_speed = 5
 		self.x_speed = random.choice([2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8])
 		self.y_speed = self.max_speed - self.x_speed
 		self.x_speed *= random.choice([-1, 1]) 
 		self.y_speed *= random.choice([-1, 1])
 		
-	def recenter(self):
-		if self.x < self.left_boundary or self.x > self.right_boundary:
-			self.x = 400
-			self.y = 268
-			leftpaddle.y, rightpaddle.y = 250, 250
-
-			self.new_speed()
 
 
 class text:
@@ -132,6 +127,7 @@ class text:
 		self.str_score = str_score
 		self.int_score = int_score
 
+
 	def convert_score_types(self):
 		self.str_score = str(self.int_score)
 
@@ -141,29 +137,36 @@ class text:
 		screen.blit(self.score, (self.x, self.y))
 
 
+
 def check_update_score():
 	if ball.x < ball.left_boundary:
 		right_score.int_score += 1
-		
 	if ball.x > ball.right_boundary:
 		left_score.int_score += 1
-		 
 
+def check_recenter():
+	if ball.x < ball.left_boundary or ball.x > ball.right_boundary:
+		ball.x = 400
+		ball.y = 268
+		leftpaddle.y, rightpaddle.y = 250, 250
+
+		ball.new_speed()
+
+	
 
 fpsClock = pygame.time.Clock()
 fps = 60
 color = (50, 164, 168)
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-left_int_score = 0
-right_int_score = 0
-
 
 ball_max_speed = 5
 ball_x_speed = random.choice([2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8])
 ball_y_speed = ball_max_speed - ball_x_speed
 ball_x_speed *= random.choice([-1, 1]) 
 ball_y_speed *= random.choice([-1, 1])
+
+
 
 ball = Ball(400, 268, ball_max_speed, ball_x_speed, ball_y_speed, 0, 568, 0, 768, [i for i in range(round(400), round(400) + 32)], 'pongball.png')
 leftpaddle = paddle(30, 250, 2, 0, 536, [i for i in range(round(250), round(250) + 64)], 'paddle.png')
@@ -182,9 +185,9 @@ while running:
 
 	screen.fill(color)
 
-	leftpaddle.load_picture()
-	rightpaddle.load_picture()
-	ball.load_picture()
+	leftpaddle.load_image()
+	rightpaddle.load_image()
+	ball.load_image()
 
 	left_score.convert_score_types()
 	left_score.load_score()
@@ -201,21 +204,20 @@ while running:
 	rightpaddle.check_pressed_keys()
 	rightpaddle.move_hitbox()
 
-	ball.check_boundary()
+	ball.check_wall_bounce()
 	ball.check_hitbox()
 	ball.move()
 	ball.move_hitbox()
 
 
 	check_update_score()
-	
-		
-	ball.recenter()
-
+	check_recenter()
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	
+	
 
 	pygame.display.update()
 	fpsClock.tick(fps)
